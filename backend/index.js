9a2f5f79ec4a89542e647794692c425462fb0af2
Ayan Sheikh <114312039+Ayan-sh03@users.noreply.github.com>
2023-07-01 17:07:21 +0530

    Update index.js
    Signed-off-by: Ayan Sheikh <114312039+Ayan-sh03@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,38 @@
 const express = require("express");
 const router = require("./Router/routes");
-const User = require("./db/User");
+const mongoose = require("mongoose");
+const userSchema = require("./Models/Users");
 const app = express();
 const PORT = 3000;
 
+const User = mongoose.model("User", userSchema);
 app.use("/api/v1/", router); //basic routes
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+const MONGO_URI = "mongodb://127.0.0.1:27017/Spendify";
+
+const start = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log("Server is listening", PORT));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// const newUser = new User({
+//   name: "John Doe",
+//   email: "johndoe@example.com",
+//   password: "23245",
+// });
+
+// // Save the new user to the database
+// newUser.save().then((res) => console.log(res));
+
+// console.log("User added successfully!");
+
+start();
