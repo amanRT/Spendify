3c44e3f48176a59df48235f8e56eb8c66d2878b7
Ayan Sheikh <114312039+Ayan-sh03@users.noreply.github.com>
2023-07-01 15:57:36 +0530

    update#2

---
+++ backend/Router/routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.route("/login"); // for login
+router.route("/register"); //for register
+
+router.route("/bill"); // for bill queries
+
+router.route("/analytics"); // for bbudget analytics
