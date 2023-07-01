ccb97ceea39a49ac134e0fd6fde1911298975bf6
Ayan Sheikh <114312039+Ayan-sh03@users.noreply.github.com>
2023-07-01 16:57:20 +0530

    schema

---
+++ backend/Models/Users.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name Required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Need mail"],
+  },
+  password: {
+    type: String,
+    required: [true, "Need Password"],
+  },
+});
+module.exports = userSchema;
