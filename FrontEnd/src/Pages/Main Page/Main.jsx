886be6f8ec623b1b4e1bf2e60d40722431ee3180
Aman Thakur <67583037+amanRT@users.noreply.github.com>
2023-07-02 22:35:32 +0530

    Final

--- FrontEnd/src/Pages/Main Page/Main.jsx
+++ FrontEnd/src/Pages/Main Page/Main.jsx
@@ -2,6 +2,7 @@ import "./Main.css";
 import NavBar from "../../Utility/NavBar";
 import Expense from "./MP-Sections/Expense";
 import Analytics from "./MP-Sections/Analytics";
+import Ocr from "./MP-Sections/Ocr";
 
 export default function Main() {
   return (
@@ -9,6 +10,7 @@ export default function Main() {
       <NavBar />
       <Expense />
       <Analytics />
+      <Ocr />
     </>
   );
 }
